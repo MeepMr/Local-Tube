7b7f4fd7dbf65fa8c8ea2ec853863a5ccb31cb8a
justinklein <jus.klein@icloud.com>
2021-05-24 21:37:17 +0200

    Reconstructed the view-page

--- routes/watch.js
+++ routes/watch.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const videoData = require('../data/dataManager');
 
-router.get('/:videoId', function(req, res) {
+router.get('/:videoId/', function(req, res) {
 
   let {videoId} = req.params;
   videoId = decodeURIComponent(videoId);
 
-  res.render('video', {title:videoId, videoId:videoId});
+  if(videoData.findVideo(videoId)) {
+
+    res.sendFile(`${videoData.videoDirectory}/${videoId}.mp4`);
+  }
 });
 
 module.exports = router;
