708f97ae7648f085bec3b38b4bad61b516bf62f0
justinklein <jus.klein@icloud.com>
2021-05-24 16:48:17 +0200

    Added the possibility to name the download

--- routes/download.js
+++ routes/download.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const ytdl = require('../bin/getYouTube-dl');
 
-router.get('/:vid/', function(req, res, next) {
+router.get('/:vid/:name?/', function(req, res) {
 
-    let {vid} = req.params;
+    let {vid, name} = req.params;
     vid = decodeURIComponent(vid);
+    name = name ? decodeURIComponent(name) : vid;
 
     /* response attachment for triggering download instead of stream */
-    res.attachment(`${vid}.mp4`);
+    res.attachment(`${name}.mp4`);
 
     ytdl(vid, res);
 });
