8ae92aeb83738e700ab696c5c0e524fd95ec0a66
justinklein <jus.klein@icloud.com>
2021-05-26 21:26:20 +0200

    Code-CleanUp 🧹

--- routes/delete.js
+++ routes/delete.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const dataManager = require('../data/dataManager');
 
-router.get('/all/', function(req, res) {
+router.get('/all/', function (req, res) {
 
     dataManager.deleteAllVideos();
 
     res.send('deleted all videos');
 });
 
-router.get('/:vid/', function(req, res) {
+router.get('/:vid/', function (req, res) {
 
     let {vid: videoId} = req.params;
     videoId = decodeURIComponent(videoId);
