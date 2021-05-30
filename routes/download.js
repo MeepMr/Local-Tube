42eee8770e95059f9398aeba2bdc39a32c7d6177
justinklein <jus.klein@icloud.com>
2021-05-30 15:02:53 +0200

    Prepared the /download/ domain for future enhancements

--- routes/download.js
+++ routes/download.js
@@ -11,24 +11,46 @@ router.get('/:videoId/:name?/', async function (req, res) {
     videoId = decodeURIComponent(videoId);
     name = name ? decodeURIComponent(name) : videoId;
 
-    /* response attachment for triggering download instead of stream */
-    res.attachment(`${name}.mp4`);
-
     if (dataManager.findVideo(videoId) !== -1) {
 
-        res.sendFile(`${dataManager.videoDirectory}/${name}.mp4`);
+        sendDownloadedVideo(res, videoId, name, true);
+
     } else {
 
-        await youTubeDl(videoId, `${dataManager.videoDirectory}/temp/${name}`);
-
-        res.sendFile(`${dataManager.videoDirectory}/temp/${name}.mp4`);
+        await youTubeDl(videoId, `${dataManager.videoDirectory}/temp/${videoId}`).catch(error => `YouTube-Dl errored with ${error}`);
+        res.redirect(`/download/file/${videoId}/${name}`);
 
         //Delete the downloaded file after the time specified in the config-File in seconds
         setTimeout(() => {
 
-            fs.unlinkSync(`${dataManager.videoDirectory}/temp/${name}.mp4`);
+            fs.unlinkSync(`${dataManager.videoDirectory}/temp/${videoId}.mp4`);
         }, dataManager.tempDuration * 1000);
     }
 });
+
+router.get('/file/:videoId/:name/', function (req, res) {
+
+    let {videoId, name} = req.params;
+    videoId = decodeURIComponent(videoId);
+    name = decodeURIComponent(name);
+
+    sendDownloadedVideo(res, videoId, name, false);
+});
+
+/**
+ * @param res {Response}
+ * @param videoId {String}
+ * @param fileName {String}
+ * @param registered {Boolean} Was the Video already registered?
+ */
+let sendDownloadedVideo = function(res, videoId, fileName, registered) {
+
+    res.attachment(`${fileName}.mp4`);
+
+    if(registered)
+        res.sendFile(`${dataManager.videoDirectory}/${videoId}.mp4`);
+    else
+        res.sendFile(`${dataManager.videoDirectory}/temp/${videoId}.mp4`);
+};
 
 module.exports = router;
