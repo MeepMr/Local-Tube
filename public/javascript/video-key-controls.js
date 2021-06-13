af5a7d746a39599b34e44e69a5872ec77e2bf823
justinklein <jus.klein@icloud.com>
2021-06-13 21:55:48 +0200

    Extracted the video-controls into a separate file

---
+++ public/javascript/video-key-controls.js
@@ -0,0 +1,89 @@
+let pip = false;
+let fullscreen = false;
+
+video.addEventListener('enterpictureinpicture', () => {pip = true;});
+video.addEventListener('leavepictureinpicture', () => {pip = false;});
+document.addEventListener('webkitfullscreenchange', () => {fullscreen = !fullscreen;});
+document.addEventListener('fullscreenchange', () => {fullscreen = !fullscreen;});
+
+window.addEventListener('keypress', async function (key) {
+
+    let keySymbol = key.key.toUpperCase();
+
+    if (keySymbol === 'M')
+        video.muted = !video.muted;
+    else if (keySymbol === ' '){
+
+        key.preventDefault();
+        if (video.paused)
+            await video.play();
+        else
+            await video.pause();
+    } else if (keySymbol === 'F') {
+
+        await toggleFullscreen();
+    } else if (keySymbol === 'P') {
+
+        await togglePip();
+    } else if (keySymbol === 'N') {
+
+        if (key.shiftKey && !key.metaKey && nextVideoLink) {
+
+            if(key.ctrlKey) {
+
+                await fetch(`/delete/${videoId}`);
+                nextVideoLink.click();
+            } else {
+
+                nextVideoLink.click();
+            }
+        }
+    }
+});
+
+window.addEventListener('keydown', function (key) {
+
+    if (key.code === 'ArrowLeft') { // Left-Arrow
+
+        video.currentTime = video.currentTime-10;
+    } else if (key.code === 'ArrowRight') { // Right-Arrow
+
+        video.currentTime = video.currentTime+10;
+    } else if(key.code === 'Delete' && key.ctrlKey) {
+
+        document.getElementsByClassName('delete-button').item(0).click();
+    }
+});
+
+video.addEventListener('dblclick', async function () {
+
+    await toggleFullscreen();
+});
+
+async function toggleFullscreen () {
+
+    if(pip)
+        await togglePip();
+
+    if (video.requestFullscreen) {
+        if(fullscreen)
+            await video.exitFullscreen();
+        else
+            await video.requestFullscreen();
+    } else {
+
+        /* Safari */
+        if (fullscreen)
+            await video.webkitExitFullscreen();
+        else
+            await video.webkitRequestFullscreen();
+    }
+}
+
+async function togglePip () {
+
+    if(pip)
+        await document.exitPictureInPicture();
+    else
+        await video.requestPictureInPicture();
+}
--- public/javascript/watchScript.js
+++ public/javascript/watchScript.js
@@ -5,15 +5,9 @@ const nextVideoLink = getNextVideo();
 const videoId = video.id;
 let duration;
 let started = false;
-let pip = false;
-let fullscreen = false;
 
 video.addEventListener('playing', Loop);
 video.addEventListener('loadedmetadata', logMetaData);
-video.addEventListener('enterpictureinpicture', () => {pip = true;});
-video.addEventListener('leavepictureinpicture', () => {pip = false;});
-document.addEventListener('webkitfullscreenchange', () => {fullscreen = !fullscreen;});
-document.addEventListener('fullscreenchange', () => {fullscreen = !fullscreen;});
 
 /**
  * @returns {HTMLVideoElement}
@@ -41,60 +35,6 @@ function getNextVideo () {
         return false;
     }
 }
-
-window.addEventListener('keypress', async function (key) {
-
-    let keySymbol = key.key.toUpperCase();
-
-    if (keySymbol === 'M')
-        video.muted = !video.muted;
-    else if (keySymbol === ' '){
-
-        key.preventDefault();
-        if (video.paused)
-            await video.play();
-        else
-            await video.pause();
-    } else if (keySymbol === 'F') {
-
-        await toggleFullscreen();
-    } else if (keySymbol === 'P') {
-
-        await togglePip();
-    } else if (keySymbol === 'N') {
-
-        if (key.shiftKey && !key.metaKey && nextVideoLink) {
-
-            if(key.ctrlKey) {
-
-                await fetch(`/delete/${videoId}`);
-                nextVideoLink.click();
-            } else {
-
-                nextVideoLink.click();
-            }
-        }
-    }
-});
-
-window.addEventListener('keydown', function (key) {
-
-    if (key.code === 'ArrowLeft') { // Left-Arrow
-
-        video.currentTime = video.currentTime-10;
-    } else if (key.code === 'ArrowRight') { // Right-Arrow
-
-        video.currentTime = video.currentTime+10;
-    } else if(key.code === 'Delete' && key.ctrlKey) {
-
-        document.getElementsByClassName('delete-button').item(0).click();
-    }
-});
-
-video.addEventListener('dblclick', async function () {
-
-    await toggleFullscreen();
-});
 
 async function logSecondsWatched () {
 
@@ -130,32 +70,4 @@ function delay (delay) {
         setTimeout(resolve, delay);
     });
 
-}
-
-async function toggleFullscreen () {
-
-    if(pip)
-        await togglePip();
-
-    if (video.requestFullscreen) {
-        if(fullscreen)
-            await video.exitFullscreen();
-        else
-            await video.requestFullscreen();
-    } else {
-
-        /* Safari */
-        if (fullscreen)
-            await video.webkitExitFullscreen();
-        else
-            await video.webkitRequestFullscreen();
-    }
-}
-
-async function togglePip () {
-
-    if(pip)
-        await document.exitPictureInPicture();
-    else
-        await video.requestPictureInPicture();
 }
