b1143dd0657e59e358a730327c2534957e70067d
justinklein <jus.klein@icloud.com>
2021-06-14 19:21:38 +0200

    Clicking on a "Next-Video" will now delete the current one, if it was watched 97%

---
+++ public/javascript/next-videos-events.js
@@ -0,0 +1,28 @@
+/** @type {HTMLCollection.<HTMLAnchorElement>} */
+const nextVideoLinks = document.getElementsByClassName('videoLink');
+
+/** @type {HTMLAnchorElement} */
+let anchorElement;
+
+for(anchorElement of nextVideoLinks) {
+
+    anchorElement.addEventListener('click', nextVideoListener);
+}
+
+/**
+ * @param mouseEvent {MouseEvent}
+ */
+async function nextVideoListener (mouseEvent) {
+
+    if(mouseEvent.button === 0) {
+
+        mouseEvent.preventDefault();
+        const targetLink = mouseEvent.target;
+
+        if((video.currentTime / video.duration) > 0.97) {
+            await fetch(`/delete/${videoId}`);
+        }
+
+        window.location.href = targetLink.href;
+    }
+}
